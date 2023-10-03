4ca2b865752eaa23e71fc44e7a17cf47325fa766
ki-ask <site@kiask.xyz>
2023-10-04 03:51:27 +0600

    Changed Full Repo

---
+++ next-seo.config.js
@@ -0,0 +1,16 @@
+const seoConfig = {
+    title: 'Markdownblog',
+    description: 'Explore the consequences of a post-Earth scenario on Earther. Discover insights into ecological changes, human survival challenges, and ethical dilemmas in a world altered by environmental crises. Join us in envisioning a sustainable future.',
+    openGraph: {
+        type: 'website',
+        locale: 'en_IE',
+        url: 'https://earther.vercel.app/',
+        site_name: 'Earther',
+    },
+    twitter: {
+        handle: '@decentrealizedw',
+        site: '@decentrealizedw',
+        cardType: 'summary_large_image',
+    },
+};
+export default seoConfig;
